fdeadee3727ff1435fd9d3085335b61eddb63b90
Denton123 <15626204923@163.com>
2019-06-05 15:15:28 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,31 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			console.log(process.env.NODE_ENV)
+			// #ifdef APP-PLUS
+			var appid = plus.runtime.appid;
+			console.log('应用的 appid 为：' + appid);
+			// #endif
+			console.log(Window)
+			switch (uni.getSystemInfoSync().platform) {
+				case 'android':
+					console.log('运行在Android上')
+					break;
+				default:
+					console.log('运行在开发者工具上')
+					break;
+			}
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
